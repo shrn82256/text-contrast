8febe03a95b259aaa168d2e4457fd03a8ffdfbc4
shrn82256 <shrn82256@gmail.com>
2019-01-28 23:04:54 +0530

    Updates

---
+++ precache-manifest.cde79bf07479d58b5761d2828d819351.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "738c163304e1069a8842",
+    "url": "/text-contrast/static/js/runtime~main.738c1633.js"
+  },
+  {
+    "revision": "51fa0282c0b196927fc4",
+    "url": "/text-contrast/static/js/main.51fa0282.chunk.js"
+  },
+  {
+    "revision": "993512540c6da52e924c",
+    "url": "/text-contrast/static/js/1.99351254.chunk.js"
+  },
+  {
+    "revision": "51fa0282c0b196927fc4",
+    "url": "/text-contrast/static/css/main.bec12bad.chunk.css"
+  },
+  {
+    "revision": "693d0976832469bb76cfddba6cc3c230",
+    "url": "/text-contrast/index.html"
+  }
+];
